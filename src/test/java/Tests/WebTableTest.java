d3b48f14934afc762a5463451e1ac7621dfe9552
acristina <mrsn.andreea@mail.com>
2024-12-03 15:05:01 +0200

    Selenium Commit

--- src/test/java/Tests/WebTableTest.java
+++ src/test/java/Tests/WebTableTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.nio.file.WatchEvent;
+
 public class WebTableTest {
 
     public WebDriver driver;
@@ -18,6 +20,39 @@ public class WebTableTest {
 
         WebElement elementsField=driver.findElement(By.xpath("//h5[text()='Elements']"));
         elementsField.click();
+
+        WebElement webTableField=driver.findElement(By.xpath("//span[text()='Web Tables']"));
+        webTableField.click();
+
+        WebElement addField=driver.findElement(By.id("addNewRecordButton"));
+        addField.click();
+
+        WebElement firstNameField=driver.findElement(By.id("firstName"));
+        String firstNameValue="Doe";
+        firstNameField.sendKeys(firstNameValue);
+
+        WebElement lastNameField=driver.findElement(By.id("lastName"));
+        String lastNameValue="Jane";
+        lastNameField.sendKeys(lastNameValue);
+
+        WebElement eMailField=driver.findElement(By.id("userEmail"));
+        String eMailValue="jane.doe@email.com";
+        eMailField.sendKeys(eMailValue);
+
+        WebElement ageField=driver.findElement(By.id("age"));
+        String ageValue="35";
+        ageField.sendKeys(ageValue);
+
+        WebElement salaryField=driver.findElement(By.id("salary"));
+        String salaryValue="5000";
+        salaryField.sendKeys(salaryValue);
+
+        WebElement departmentField=driver.findElement(By.id("department"));
+        String departmentValue="IT";
+        departmentField.sendKeys(departmentValue);
+
+        WebElement submitField=driver.findElement(By.id("submit"));
+        submitField.click();
 
     }
 
